a6ee4c442b946368c76da8fe7b816f704655fd6b
kmatute393-png <kmatute393@gmail.com>
2026-09-21 21:40:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-console.log("🌹 La rosa ha florecido...");
-
-const rose = document.querySelector(".rose");
-
-rose.addEventListener("click", () => {
-
-    document.querySelector(".message h1").textContent =
-        "Para alguien especial ❤️";
-
+const boton =
+document.querySelector(".hero button");
+boton.addEventListener("click", function() {
+    const mensaje = "Hola, quiero una pagina web para mi negocio.";
+    const url = "https://wa.me/573222943826?text=" + encodeURIComponent(mensaje);
+    window.open(url, "_blank");
+});
+const botonContacto =
+document.querySelector("#botonContacto");
+botonContacto.addEventListener("click", function(){
+       const mensaje = "Hola, quiero hablar con ustedes sobre una pagina web para mi negocio.";
+    const url = "https://wa.me/573222943826?text=" + encodeURIComponent(mensaje);
+    window.open(url, "_blank");  
 });
